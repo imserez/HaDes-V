cb6bde37ebfaf0029ebb47ed1c98ec7965cb9ed3
Sergi <sergijp22@gmail.com>
2026-03-05 08:47:10 +0100

    re-defining FSM states using typedef enums

--- rtl/fetch_stage.sv
+++ rtl/fetch_stage.sv
@@ -26,26 +26,26 @@ module fetch_stage (
 
 
     logic [31:0] pc = 0;
-    logic [31:0] saved_instruction = 0;
 
     logic [1:0] saved = 0;
     logic [1:0] finished_reset = 0;
 
-    logic [2:0] curr_fetch_status = fetch_status.STAGE_INIT;
-    logic [2:0] curr_wb_status;
 
-    enum logic [1:0] {
+    typedef enum logic [1:0] {
         STAGE_RST,
         STAGE_FETCH,
         STAGE_HOLD
-    } fetch_status;
+    } fetch_state_t;
 
-    enum logic [1:0] {
+    typedef enum logic [1:0] {
         WB_SENT,
         WB_WORKING,
         WB_READY
-    } wb_status;
+    } wb_state_t;
 
+
+    logic [2:0] curr_fetch_status = fetch_state_t::STAGE_INIT;
+    logic [2:0] curr_wb_status;
 
 
     // combinational logic for backwarding status
@@ -74,7 +74,7 @@ module fetch_stage (
     always_ff @(posedge clk) begin
 
         if (rst) begin
-            curr_fetch_status <= fetch_status.STAGE_RST;
+            curr_fetch_status <= fetch_state_t::STAGE_RST;
             wb.cyc <= 0;
             wb.stb <= 0;
             finished_reset <= 0;
@@ -99,25 +99,23 @@ module fetch_stage (
                 STAGE_FETCH: begin
                     if (wb.err == 1) begin
                         status_forwards_out <= pipeline_status::FETCH_FAULT;
-                        finished_reset <= 1;
+                        // what with finished_rst here?
+
                         // at this sage, retry read? pc to next instruction?
                     end
                     else if (wb.ack == 1) begin
-                        saved_instruction <= wb.dat_miso; // miso = read!
-
-                        // could this be a timing problem if it's set at this clock cycle?
-                        // do I need to save it in a reg at all? or can assign to wb.dat_miso
+                        finished_reset <= 1;
 
                         // imagine, if we're ready, go fetch the next instruction and store the current. In case a JUMP is issued, clean our registers and read again. This time, fetch would be faster?
-                        instruction_reg_out <= saved_instruction;
+                        instruction_reg_out <= wb.dat_miso; // miso = read!
 
                         if (status_backwards_in == pipeline_status::READY) begin
-                            curr_fetch_status <= fetch_status.STAGE_FETCH;
+                            curr_fetch_status <= fetch_state_t::STAGE_FETCH;
                             status_forwards_out <= pipeline_status::BUBBLE;
                             pc <= pc + 4;
                         end
                         else begin
-                            curr_fetch_status <= fetch_status.STAGE_HOLD;
+                            curr_fetch_status <= fetch_state_t::STAGE_HOLD;
                             status_forwards_out <= pipeline_status::VALID;
                             wb.cyc <= 0;
                             wb.stb <= 0;
@@ -127,7 +125,7 @@ module fetch_stage (
                 STAGE_HOLD: begin
                     if (status_backwards_in == pipeline_status::READY) begin
                         pc <= pc + 4;
-                        curr_fetch_status <= fetch_status.STAGE_FETCH;
+                        curr_fetch_status <= fetch_state_t::STAGE_FETCH;
                         status_forwards_out <= pipeline_status::BUBBLE;
                         // good idea to load everything here to save 1 clk cycle?
                         // or do it in next FSM check?
@@ -144,87 +142,6 @@ module fetch_stage (
             program_counter_reg_out <= pc;
         end
     end
-
-
-
-    // always_ff @(posedge clk) begin
-
-    //     if(rst) begin
-    //         pc <= 0;
-    //         after_reset <= 1;
-    //         wb.cyc <= 0;
-    //         wb.stb <= 0;
-    //     end
-    //     else begin
-
-    //         // init ask
-    //         if (wb.ack == 1 && (status_backwards_in != pipeline_status::READY)) begin
-    //             // if we have the response, and the next stage is not ready for it
-    //             // then we hold this value.
-    //             if (!saved) begin
-
-    //                 read_instruction <= wb.dat_miso;
-    //                 saved <= 1;
-    //             end
-    //             wb.cyc <= 0;
-    //             wb.stb <= 0;
-    //             wb.we  <= 0;
-    //         end
-    //         else begin
-    //             wb.cyc <= 1;
-    //             wb.stb <= 1;
-    //             wb.we  <= 0;
-    //             saved <= 0;
-    //         end
-
-    //         // 4-bytes of the word are accessed
-
-    //         // set the apropiate address to fetch from
-    //         // if (after_reset == 1) begin
-    //         //     wb.adr <= constants::RESET_ADDRESS >> 2;
-    //         //     pc <= constants::RESET_ADDRESS;
-    //         // end
-    //         // else if (status_backwards_in == pipeline_status::JUMP) begin
-    //         //     wb.adr <= jump_address_backwards_in >> 2;
-    //         //     pc <= jump_address_backwards_in;
-    //         // end
-    //         // else begin
-    //         //     wb.adr <= pc >> 2; // byte-addressing to word-addressing
-    //         //     program_counter_reg_out <= pc;
-    //         // end
-
-    //         program_counter_reg_out <= pc;
-
-
-    //         // wait for response
-    //         if (wb.ack == 1) begin
-    //             after_reset <= 0;
-
-    //             read_instruction <= wb.dat_miso;
-
-    //             instruction_reg_out <= read_instruction; // read => out (mIsO)
-    //             status_forwards_out <= pipeline_status::VALID;
-
-    //             if (status_backwards_in == pipeline_status::READY) begin
-    //                 pc <= pc + 4;
-    //             end
-
-    //         end
-    //         else begin
-    //             if (wb.err == 1) begin
-    //                 status_forwards_out <= pipeline_status::FETCH_FAULT;
-    //             end
-    //             else begin
-    //                 status_forwards_out <= pipeline_status::BUBBLE;
-    //             end
-    //         end
-
-
-
-    //     end
-    // end
-
-
 
 
     // // TODO: Delete the following line and implement this module.
