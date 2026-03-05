c9efd067120f3b5c289f9e406a0756d3ca8fba65
Sergi <sergijp22@gmail.com>
2026-03-05 09:14:34 +0100

    Clearing FSM Implementation, now, respecting the JUMP priorities and also using some always_comb logic to modify the wb.adr

--- rtl/fetch_stage.sv
+++ rtl/fetch_stage.sv
@@ -44,16 +44,14 @@ module fetch_stage (
     } wb_state_t;
 
 
-    logic [2:0] curr_fetch_status = fetch_state_t::STAGE_INIT;
+    logic [2:0] curr_fetch_status = fetch_state_t::STAGE_RST;
     logic [2:0] curr_wb_status;
 
 
     // combinational logic for backwarding status
     always_comb begin
-
+        wb.adr = pc >> 2;    // interesting, comb. logic
     end
-
-    // TODO: change enum to typedef
     // check if finished_reset makes sense
 
 
@@ -74,70 +72,64 @@ module fetch_stage (
     always_ff @(posedge clk) begin
 
         if (rst) begin
-            curr_fetch_status <= fetch_state_t::STAGE_RST;
+            curr_fetch_status <= fetch_state_t::STAGE_FETCH;
             wb.cyc <= 0;
             wb.stb <= 0;
             finished_reset <= 0;
+            pc <= constants::RESET_ADDRESS;
             //TODO: maybe add here forwarding state? Bubble?
+            //TODO: implement fetch_misaligned? %4?
         end
         else begin
 
             if (status_backwards_in == pipeline_status::JUMP && finished_reset) begin
-                wb.adr <= jump_address_backwards_in >> 2;
                 pc <= jump_address_backwards_in;
+                status_forwards_out <= pipeline_status::BUBBLE;
             end
-            else if (finished_reset) begin
-                wb.adr <= pc >> 2;
-            end
-
-            case (curr_fetch_status)
-                STAGE_RST: begin
-                    curr_fetch_status <= STAGE_FETCH;
-                    wb.adr  <= constants::RESET_ADDRESS >> 2;
-                    pc <= constants::RESET_ADDRESS;
-                end
-                STAGE_FETCH: begin
-                    if (wb.err == 1) begin
-                        status_forwards_out <= pipeline_status::FETCH_FAULT;
-                        // what with finished_rst here?
-
-                        // at this sage, retry read? pc to next instruction?
-                    end
-                    else if (wb.ack == 1) begin
-                        finished_reset <= 1;
-
-                        // imagine, if we're ready, go fetch the next instruction and store the current. In case a JUMP is issued, clean our registers and read again. This time, fetch would be faster?
-                        instruction_reg_out <= wb.dat_miso; // miso = read!
-
-                        if (status_backwards_in == pipeline_status::READY) begin
-                            curr_fetch_status <= fetch_state_t::STAGE_FETCH;
-                            status_forwards_out <= pipeline_status::BUBBLE;
-                            pc <= pc + 4;
-                        end
-                        else begin
-                            curr_fetch_status <= fetch_state_t::STAGE_HOLD;
-                            status_forwards_out <= pipeline_status::VALID;
-                            wb.cyc <= 0;
-                            wb.stb <= 0;
-                        end
-                    end
-                end
-                STAGE_HOLD: begin
-                    if (status_backwards_in == pipeline_status::READY) begin
-                        pc <= pc + 4;
-                        curr_fetch_status <= fetch_state_t::STAGE_FETCH;
-                        status_forwards_out <= pipeline_status::BUBBLE;
-                        // good idea to load everything here to save 1 clk cycle?
-                        // or do it in next FSM check?
+            else begin
+                case (curr_fetch_status)
+                    STAGE_FETCH: begin
                         wb.cyc <= 1;
                         wb.stb <= 1;
-                    end
-                end
-            endcase
+                        if (wb.err == 1) begin
+                            status_forwards_out <= pipeline_status::FETCH_FAULT;
+                            // what with finished_rst here?
 
-            // this FSM implementation could be more clear if every state implements at it's own time its properties, but this would cost 1 more clk cycle.
-            // current: clk[0] => change+set clk[1] = ready
-            // clearer: clk[0] => change => clk[1] => set., clk[2] ready
+                            // at this sage, retry read? pc to next instruction?
+                        end
+                        else if (wb.ack == 1) begin
+                            finished_reset <= 1;
+
+                            // imagine, if we're ready, go fetch the next instruction and store the current. In case a JUMP is issued, clean our registers and read again. This time, fetch would be faster?
+                            instruction_reg_out <= wb.dat_miso; // miso = read!
+
+                            if (status_backwards_in == pipeline_status::READY) begin
+                                curr_fetch_status <= fetch_state_t::STAGE_FETCH;
+                                status_forwards_out <= pipeline_status::VALID;
+                                pc <= pc + 4;
+                            end
+                            else begin
+                                curr_fetch_status <= fetch_state_t::STAGE_HOLD;
+                                status_forwards_out <= pipeline_status::VALID;
+                                wb.cyc <= 0;
+                                wb.stb <= 0;
+                            end
+                        end
+                        else begin
+                            status_forwards_out <= pipeline_status::BUBBLE;
+                        end
+                    end
+                    STAGE_HOLD: begin
+                        if (status_backwards_in == pipeline_status::READY) begin
+                            pc <= pc + 4;
+                            curr_fetch_status <= fetch_state_t::STAGE_FETCH;
+                            status_forwards_out <= pipeline_status::BUBBLE;
+                            wb.cyc <= 1;
+                            wb.stb <= 1;
+                        end
+                    end
+                endcase
+            end
 
             program_counter_reg_out <= pc;
         end
