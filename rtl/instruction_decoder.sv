c9ac9945ce8833a45da874e534113854ef47b651
Sergi <sergijp22@gmail.com>
2026-03-09 10:31:08 +0100

    B-type imm !

--- rtl/instruction_decoder.sv
+++ rtl/instruction_decoder.sv
@@ -23,7 +23,12 @@ module instruction_decoder (
 
 
     always_comb begin
-
+        instruction_out.op          = op::ILLEGAL;
+        instruction_out.rd_address  = 5'b0;
+        instruction_out.rs1_address = 5'b0;
+        instruction_out.rs2_address = 5'b0;
+        instruction_out.immediate   = 32'b0;
+        // instruction_out.csr         = csr::     ;
 
         case (instruction_in[6:0])
 
@@ -31,14 +36,15 @@ module instruction_decoder (
                 instruction_out.rd_address  = instruction_in[11:7];
                 instruction_out.rs1_address = instruction_in[19:15];
                 instruction_out.rs2_address = instruction_in[24:20];
-                instruction_out.immediate   = 0;
+                instruction_out.immediate   = 32'b0;
+                // instruction_out.csr         = csr::     ;
 
                 case ({instruction_in[31:25], instruction_in[14:12]})
                     {7'b0000000, 3'b000}: instruction_out.op = op::ADD;
                     {7'b0100000, 3'b000}: instruction_out.op = op::SUB;
                     {7'b0000000, 3'b001}: instruction_out.op = op::SLL;
                     {7'b0000000, 3'b010}: instruction_out.op = op::SLT;
-                    {7'b0000000, 3'b011}: instruction_out.op = op::SLTU
+                    {7'b0000000, 3'b011}: instruction_out.op = op::SLTU;
                     {7'b0000000, 3'b100}: instruction_out.op = op::XOR;
                     {7'b0000000, 3'b101}: instruction_out.op = op::SRL;
                     {7'b0100000, 3'b101}: instruction_out.op = op::SRA;
@@ -47,11 +53,12 @@ module instruction_decoder (
                     default: instruction_out.op = op::ILLEGAL;
                 endcase
             end
-             7'b0010011: begin // I-TYPE
+            7'b0010011: begin // I-TYPE
                 instruction_out.rd_address  = instruction_in[11:7];
                 instruction_out.rs1_address = instruction_in[19:15];
                 instruction_out.rs2_address = 0;
                 instruction_out.immediate   = { {20{instruction_in[31]}}, instruction_in[31:20] }; // Sign-extending!!
+                // instruction_out.csr         = csr::     ;
 
                 casez ({instruction_in[31:20], instruction_in[14:12]})
                     {12'b????????????, 3'b000}: instruction_out.op = op::ADDI;
@@ -65,7 +72,22 @@ module instruction_decoder (
                     {12'b0100000?????, 3'b101}: instruction_out.op = op::SRAI;
                     default: instruction_out.op = op::ILLEGAL;
                 endcase
-             end
+            end
+            7'b0100011: begin // S-TYPE
+            end
+            7'b1100011: begin // B-TYPE
+                instruction_out.rd_address  = 5'b0;
+                instruction_out.rs1_address = instruction_in[19:15];
+                instruction_out.rs2_address = instruction_in[24:20];
+                instruction_out.immediate = {
+                    {20{instruction_in[31]}},
+                    instruction_in[7],
+                    instruction_in[30:25],
+                    instruction_in[11:8],
+                    1'b0
+                };
+                // instruction_out.csr         = csr::     ;
+            end
 
 
 
