a74bb0598f7feff32806d8a4d06ac71cb2f13b75
Sergi <sergijp22@gmail.com>
2026-03-09 10:07:25 +0100

    keep advancing instruction_decoder

--- rtl/instruction_decoder.sv
+++ rtl/instruction_decoder.sv
@@ -23,9 +23,43 @@ module instruction_decoder (
 
 
     always_comb begin
+
+
+        case (instruction_in[6:0])
+
+            7'b0110011: begin // R-TYPE
+                instruction_out.rd_address = instruction_in[11:7];
+                instruction_out.rs1_address = instruction_in[19:15];
+                instruction_out.rs2_address = instruction_in[24:20];
+
+                case ({instruction_in[31:25], instruction_in[14:12]})
+
+                    {7'b0000000, 3'b000}: instruction_out.op = op::ADD;
+                    {7'b0100000, 3'b000}: instruction_out.op = op::SUB;
+
+                endcase
+
+                case(instruction_in[14:12])
+
+                    3'b001: instruction_out.op = op.ADD;
+                    3'b010: instruction_out.op = op.ADD;
+                    3'b011: instruction_out.op = op.ADD;
+                    3'b100: instruction_out.op = op.ADD;
+                    3'b110: instruction_out.op = op.ADD;
+                    3'b111: instruction_out.op = op.ADD;
+
+
+                endcase
+            end
+
+
+
+        endcase
+
+
         casez(instruction_in)
 
-            {22'b?, 5'b00000, 7'b0110011}: begin
+            {25'b?, 7'b0110011}: begin
 
             end
 
