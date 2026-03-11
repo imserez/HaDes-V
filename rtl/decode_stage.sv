5084ff819345af97be921d0983530df7c5df961b
Sergi <sergijp22@gmail.com>
2026-03-11 13:50:33 +0100

    Finally found itmake test/sv/fetch/test_fetch ! I needed to preserve all current states in case of backwards_in STALL. Time to decodelazygit Finally

--- rtl/decode_stage.sv
+++ rtl/decode_stage.sv
@@ -33,68 +33,68 @@ module decode_stage (
     output logic [31:0] jump_address_backwards_out
 );
 
-    // import decode_status::*;
+    import decode_status::*;
 
-    // instruction::t  decoded_instruction;
+    instruction::t  decoded_instruction;
 
-    // instruction_decoder decoder (
-    //     .instruction_in(instruction_in),
-    //     .instruction_out(decoded_instruction)
-    // );
+    instruction_decoder decoder (
+        .instruction_in(instruction_in),
+        .instruction_out(decoded_instruction)
+    );
 
-    // register_file   reg_file (
-    //     .clk(clk),
-    //     .rst(rst),
-    //     .read_address1(decoded_instruction.rs1_address),
-    //     .read_data1(rs1_data_reg_out),
-    //     .read_address2(decoded_instruction.rs2_address),
-    //     .read_data2(rs2_data_reg_out),
-    //     .write_address(decoded_instruction.rd_address),
-    //     .write_data(0),
-    //     .write_enable(0)
-    // );
+    register_file   reg_file (
+        .clk(clk),
+        .rst(rst),
+        .read_address1(decoded_instruction.rs1_address),
+        .read_data1(rs1_data_reg_out),
+        .read_address2(decoded_instruction.rs2_address),
+        .read_data2(rs2_data_reg_out),
+        .write_address(decoded_instruction.rd_address),
+        .write_data(0),
+        .write_enable(0)
+    );
 
-    // // Forwards
-    // // assign jump_address_backwards_out   = jump_address_backwards_in;
+    // Forwards
+    // assign jump_address_backwards_out   = jump_address_backwards_in;
 
-    // // Backwards
-    // assign jump_address_backwards_out = jump_address_backwards_in
-
-
-
-    // always_ff @(posedge clk) begin
-
-    //     if (rst) begin
-    //         // Output Registers
-    //         rs1_data_reg_out <= 0;
-    //         rs2_data_reg_out <= 0;
-    //         instruction_reg_out <= 0;
-    //         program_counter_reg_out <= 0;
-    //         // Pipeline Control
-    //         status_backwards_out <= pipeline_status::READY;
-    //         status_forwards_out <= pipeline_status::BUBBLE;
-    //         // ----------
-    //         curr_dec_status <= STAGE_WAIT;
-    //     end
-    //     else begin
-    //         case (curr_dec_status)
-
-    //             STAGE_WAIT: begin
-    //                 status_backwards_out <= pipeline_status::READY;
-    //                 if (status_forwards_in == pipeline_status::VALID) begin
-    //                     program_counter_reg_out <= program_counter_in;
-    //                     instruction_reg_out <= decoded_instruction;
-    //                 end
-    //             end
-    //         endcase
-
-    //     end
+    // Backwards
+    assign jump_address_backwards_out = jump_address_backwards_in
 
 
-    // end
+
+    always_ff @(posedge clk) begin
+
+        if (rst) begin
+            // Output Registers
+            rs1_data_reg_out <= 0;
+            rs2_data_reg_out <= 0;
+            instruction_reg_out <= 0;
+            program_counter_reg_out <= 0;
+            // Pipeline Control
+            status_backwards_out <= pipeline_status::READY;
+            status_forwards_out <= pipeline_status::BUBBLE;
+            // ----------
+            curr_dec_status <= STAGE_WAIT;
+        end
+        else begin
+            case (curr_dec_status)
+
+                STAGE_WAIT: begin
+                    status_backwards_out <= pipeline_status::READY;
+                    if (status_forwards_in == pipeline_status::VALID) begin
+                        program_counter_reg_out <= program_counter_in;
+                        instruction_reg_out <= decoded_instruction;
+                    end
+                end
+            endcase
+
+        end
+
+
+    end
 
 
     // // TODO: Delete the following line and implement this module.
-    ref_decode_stage golden(.*);
+    // ref_decode_stage golden(.*);
 
 endmodule
